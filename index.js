5b77a459a94d165dd3dcb4c35a2419f3b1b16e95
Maximo Bautista <max.bautista97@gmail.com>
2019-08-17 21:31:26 -0400

    Polygon Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+class Polygon {
+
+  constructor(integers){
+    this.integers = integers;
+  }
+
+  get countSides(){
+    return this.integers.length;
+  }
+
+  get perimeter(){
+    return this.integers.reduce((total, integer) => total + integer);
+  }
+
+}
