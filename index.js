7963a5581933b4dcd924b932969d15c00a1be724
Maximo Bautista <max.bautista97@gmail.com>
2019-08-24 23:15:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 class Polygon {
 
-  constructor(integers){
+  constructor(integers) {
     this.integers = integers;
   }
 
-  get countSides(){
+  get countSides() {
     return this.integers.length;
   }
 
-  get perimeter(){
+  get perimeter() {
     return this.integers.reduce((total, integer) => total + integer);
   }
 
 }
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    for(let i = 0; i < this.integers.length; i++){
+      if(this.perimeter - this.integers[i] > this.integers[i] === false) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+
+  class Square extends Polygon {
+
+    get isValid() {
+      return this.perimeter % this.countSides === 0 ? true : false;
+    }
+
+    get area() {
+      return this.integers[0] * this.integers[0];
+    }
+  }
